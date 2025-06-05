1192dd410fcf1bbbab00df79e3c0db701b9a452f
meisam0083 <meisam0083@yahoo.com>
2025-06-05 20:58:53 +0330

    Update gemini-proxy.js

--- netlify/functions/gemini-proxy.js
+++ netlify/functions/gemini-proxy.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 // کلید API Gemini خود را اینجا قرار دهید
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
-const GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent";
+const GEMINI_API_URL = https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent;
 const GEMINI_VISION_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision:generateContent";
 
 exports.handler = async function(event, context) {
